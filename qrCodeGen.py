eb8446c7a543c19b76193f29afa5ed7190996561
Plumbeyy <jaredburnham516@gmail.com>
2023-06-05 21:48:23 -0500

    Initial_Base_Proj

--- qrCodeGen.py
+++ qrCodeGen.py
@@ -1,1 +1,22 @@
-print('Howdy')
+import qrcode
+import PIL
+
+def generate_qr_code(text, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="green", back_color="black")
+    img.save(file_name)
+
+
+text = "https://www.youtube.com/watch?v=ZqS_lnqSgeY&list=LL&index=2"
+
+file_name = "qr_code.png"
+
+generate_qr_code(text, file_name)
+print(f"QR code saved as {file_name}")
